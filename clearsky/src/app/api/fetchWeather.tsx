e6e697ccf84095ebfb990d6f23e2f831be3a4574
Eazybel <eazybel27@gmail.com>
2026-09-07 22:48:31 +0300

    feat: refactor fetchWeather function to improve data handling and structure DailyDataInterface

--- clearsky/src/app/api/fetchWeather.tsx
+++ clearsky/src/app/api/fetchWeather.tsx
@@ -1,17 +1,26 @@
 export default async function  fetchWeather(formData:FormData){
 const cityName=formData.get("city")
+interface DailyDataInterface{
+        day:string,
+        riskLevel:string,
+        message:string
+    }
 
+const dailyData:DailyDataInterface=[]
+const response=await fetch(`https://geocode.maps.co/search?q=${cityName}&api_key=6a9e864fde97d568213086chl07ce55`)
+const data=await response.json()
+await fetch(`https://flood-api.open-meteo.com/v1/flood?latitude=${data[0].lat}&longitude=${data[0].lon}&daily=river_discharge,river_discharge_max,river_discharge_p25,river_discharge_p75`).then(res=>{
+    return res.json()
+}).then(dataGeo=>{
 
-try{
-   
-      const response=await fetch(`https://geocode.maps.co/search?q=${cityName}&api_key=6a9e864fde97d568213086chl07ce55`)
-    const data=await response.json()
-     const responseGeo=await fetch(`https://flood-api.open-meteo.com/v1/flood?latitude=${data[0].lat}&longitude=${data[0].lon}&daily=river_discharge,river_discharge_max,river_discharge_p75,river_discharge_p90`)
-    const dataGeo=await responseGeo.json()
-    console.log(dataGeo)
-    // console.log(data[0])
-}catch(error:unknown){
-    console.log(error)
+for (let i = 0; i < dataGeo.daily.time.length; i++) {
+  dailyData.push({
+    dail
+  })
+    
 }
+
+})
+
  
 }
